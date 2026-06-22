b1b26910b2377ba8cdcd5b0c58ab06ff8bfd6a4a
trustjesustoday <lasserrem@gmail.com>
2026-06-22 07:58:52 -0500

    Delete wallpaper.sh

--- wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# command to list images with paths in current directory    ls "$PWD"/{*,.*}
-# command to create images.txt in current directory         find $(pwd) -maxdepth 1 -type f -not -path '*/\.*' | sort > images.txt
-
-var=$(sort -R images.txt | head -n1)
-swaybg -i $var
-
